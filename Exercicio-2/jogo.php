93bd439ef9b1545fdaee24c966cfea8547c05657
Jhonatan-HenriqueS <jhonatanhrcomercial@gmail.com>
2026-08-28 17:59:24 -0400

    feat: change A

--- Exercicio-2/jogo.php
+++ Exercicio-2/jogo.php
@@ -28,18 +28,14 @@ do{
                 |        Opção 0 - Sair                     |
                 |-------------------------------------------| 
             ";
-        //     $categoriaInformada = readline("Seleciona a categoria que deseja jogar: ");
-
-        //    } while(ctype_lower($categoriaInformada) !== true && $categoriaInformada != 0);
 
        $dadosCSV = extrairDados();
-
-       $categoria = readline("Informe uma categoria: ");
-       exibirCsv($dadosCSV, $categoria);
-            
+       $categoria = readline("Informe uma categoria: ");       
+       $resultadoFinal = exibirVidas(exibirCsv($dadosCSV, $categoria));
+       echo $resultadoFinal;
             break;
         case 0:
-            echo "\n Finalizado! \n";
+            echo "\n Finalizado! \n"; 
             break;
         default:
             echo "\n Opção inválida, tente novamente. \n";
@@ -49,13 +45,9 @@ do{
 
 //Atividade C: Criar uma função que existe apenas 6 vidas, ou seja, se errar 6 vezes o jogador perde
 
-function sortearPalavra ($array) {
-    return $array[array_rand($array)];
-}
-
 function exibirVidas(array $nArray){
 
-    $palavraSortida = sortearPalavra($nArray);
+    $palavraSortida = $nArray[array_rand($nArray)];
     $letraDigitada = "";
 
     $existe = "false";
@@ -64,19 +56,21 @@ function exibirVidas(array $nArray){
     $sublinhados = [];
 
         do{
+
             // Array é transformado em palavra para realizar processamento
-            foreach (str_split($palavraSortida) as $key => $letra){
-                //Se na chave encontrada não possuir nenhum valor, adiciona _
+            foreach (str_split($palavraSortida['palavra']) as $key => $letra){
+
+                // Verifica se a posição não existe
                 if (!isset($sublinhados[$key]))
                     $sublinhados[$key] = " _ ";
 
-                //Se a letra for -, adiciona no array e pula o restante do código
+                // Verifica se há hifen
                 if ($letra === "-"){
                     $sublinhados[$key] = " - ";
                     continue;
                 }   
 
-                //Se a letra for igual a letra na posição do array, o array na posição key recebe a letra 
+                // Verifica se a letra existe e exibe
                 if ($letraDigitada == $letra){
                     $sublinhados[$key] = " $letra ";
                     $existe = true;
@@ -84,73 +78,60 @@ function exibirVidas(array $nArray){
                          
             }
 
-            //Transforma o array em letras
+            // Transforma o Array em uma palavra
             $palavraImplode = implode('', $sublinhados);
 
             if (!$existe){
                 
-                //Faço a verificação da letra digitada para descontar a vida. Verifico se é letra minúscula e se ela já existe
+                // Verifica se é uma letra 
                 if (ctype_lower($letraDigitada) && !str_contains($letrasDigitadas, $letraDigitada)){ 
                     $vidasRestantes--;
-                    echo "\n Letra inválida, -1 vida \n Você possui $vidasRestantes vidas restantes!\n";
                     $letrasDigitadas .= "$letraDigitada, ";
+                    echo "\n Letra inválida, -1 vida \n Você possui $vidasRestantes vidas restantes!\n";
                 }
-            }
-            
 
-            //Troca os espaços da palavra por nenhum
-            if (str_replace(' ', '', $palavraImplode) == $palavraSortida || $vidasRestantes == 0)
-                break;
+            }
+
+            // Encerra o código caso a palavra foi encontrada ou as vidas acabaram
+            if (str_replace(' ', '', $palavraImplode) == $palavraSortida['palavra'] || $vidasRestantes == 0) break;
 
             echo $palavraImplode;  
             echo ($letrasDigitadas == "") ? "\nNenhum erro até o momento\n" : "\nLetras já usadas: $letrasDigitadas \n";
 
-            do{
-                $letraDigitada = ctype_lower(readline("Informe uma letra ou 0 para encerrar: "));
-            }while (strlen(trim($letraDigitada)) != 1 && $letraDigitada != "0");
-
+           do{
+             $letraDigitada = readline("Informe uma letra ou 0 para encerrar: ");
+           } while(strlen($letraDigitada) != 1 && $letraDigitada != "0");
+           
             $existe = false;
 
             echo "\033[2J\033[;H";
 
         } while ($letraDigitada != "0");
 
-    return $palavraSortida;
+    return $palavraSortida['palavra'];
 }
 
 //Atividade D: As palvras do jogo devem ser de outro arquivo em .csv, o usuário deve escolher a categoria e pode adicionar uma nova palavra
 
-function exibirCsv(array $nArray, $categoriaSelecionada){
-
-    $arrayCategoria = array_filter($nArray, function ($cat) {
-
-    });
-
-    if(!$arrayCategoria){
-        return "A categoria selecionada não foi encontrada!";
-    } else {
-        return $arrayCategoria;
-    }
-}
-
-//Abrir arquivo .CSV e retorna o array de todos os valores
+// Retorna os dados do arquivo csv
 
 function extrairDados(){
     $dadosExtraidos = [];
-    //Seleciona o arquivo desejado e "r" é para abrir como leitura e passa para a váriavel; Se o arquivo selecionado existe (true), execute
+
+    // Abre o arquivo
     if(($handle = fopen("Lib/data.csv", "r"))){
 
-        //Pula a primeira linha (cabeçalho)
+        // Seleciona e pula a primeria linha (caeçalho)
         $cabecalho = fgetcsv($handle, 0, ";");
 
-        //Seleciona cada coluna e seus devidos valores dentro do csv e passa para a váriavel; Enquanto exisitir palavra, execute
+        // Executa enquanto existir palavras no arquivo
         while (($fields =  fgetcsv($handle, 0, ";"))) {
 
-            //A coluna vira chave daquele valor no meu array
-            $dadosExtraidos[] = array_combine($cabecalho, $fields); 
+        // extrai os dados com chave e valores
+        $dadosExtraidos[] = array_combine($cabecalho, $fields); 
         }
 
-        //fecha o arquivo, já que ele foi aberto para leitura ou algo do tipo
+        // Encerra a leitura 
         fclose($handle);      
     } else {
         return "O diretório não existe!";
@@ -159,5 +140,15 @@ function extrairDados(){
     return $dadosExtraidos;
 }
 
-//Função que repete ate o usuário digitar uma categoria válida
+function exibirCsv(array $nArray, $categoriaSelecionada){
+
+    // Seleciona apenas a categoria selecionada 
+    $arrayCategoria = array_filter($nArray, fn($cat) => $cat['categoria'] == $categoriaSelecionada);
+
+    if(empty($arrayCategoria)){
+        return "A categoria selecionada não foi encontrada!";
+    } 
+    
+    return $arrayCategoria;
+}
 
